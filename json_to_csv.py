f76299a0b3a0db1a3c1fede333991410b5a061cc
hands <hands@email.arizona.edu>
2017-08-02 14:58:42 -0700

    Added statistics

--- json_to_csv.py
+++ json_to_csv.py
@@ -5,13 +5,13 @@ from collections import OrderedDict
 
 chdir("/home/ece411c/Documents")
 
-with open('gps_test.json', 'r') as f:
+with open('purple_dinosaur.json', 'r') as f:
     file_str = f.read()
     data = json.loads(file_str, object_pairs_hook=OrderedDict)
 
 f = csv.writer(open("test.csv", "wb+"))
 
-# Write CSV Header, If you dont need that, remove this line
+# Write CSV Header, If you don't need that, remove this line
 f.writerow(["latitude", "longitude", "event length", "event time", "event type", "gain", "gps present",
             "power","ref lock", "rx frequency", "rx rate", "rx time", "temp"])
 
