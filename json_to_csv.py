703f2cb93cde6af29ccb7e9e3505121cf5893983
hands <hands@email.arizona.edu>
2017-08-07 21:58:21 -0700

    The rest of the python files

--- json_to_csv.py
+++ json_to_csv.py
@@ -3,29 +3,33 @@ import csv
 from os import chdir
 from collections import OrderedDict
 
+# Navigate to folder with json file
 chdir("/home/ece411c/Documents")
 
-with open('purple_dinosaur.json', 'r') as f:
+# Open json file
+with open('data_Tucson_to_Phoenix.json', 'r') as f:
     file_str = f.read()
     data = json.loads(file_str, object_pairs_hook=OrderedDict)
 
-f = csv.writer(open("test.csv", "wb+"))
+# Open new csv file
+f = csv.writer(open("data_TtoP_Aug4.csv", "wb+"))
 
-# Write CSV Header, If you don't need that, remove this line
-f.writerow(["latitude", "longitude", "event length", "event time", "event type", "gain", "gps present",
-            "power","ref lock", "rx frequency", "rx rate", "rx time", "temp"])
+# Write csv header
+f.writerow(["event length", "event time", "event type", "power",
+            "frequency", "rate", "time"])
 
+# Write data
 for x in data:
-    f.writerow([x["Latitude"],
-                x["Longitude"],
+    f.writerow([#x["Latitude"],
+                #x["Longitude"],
                 x["es::event_length"],
                 x["es::event_time"],
                 x["es::event_type"],
-                x["gain"],
-                x["gps_present"],
+                #x["gain"],
+                #x["gps_present"],
                 x["power"],
-                x["ref_locked"],
+                #x["ref_locked"],
                 x["rx_freq"][1],
                 x["rx_rate"][1],
-                x["rx_time"][1][0]+x["rx_time"][1][1],
-                x["temp"]])
+                x["rx_time"][1][0]+x["rx_time"][1][1]])
+                #x["temp"] ])
